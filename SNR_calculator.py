1ae1fdcd628ac200efb4aa646ae79e1f7e4266ef
Tim171717 <tfessler@student.ethz.ch>
2025-05-08 15:22:04 +0200

    Update SNR_calculator.py

--- SNR_calculator.py
+++ SNR_calculator.py
@@ -8,7 +8,7 @@ from astroquery.gaia import Gaia
 from astroquery.mast import Catalogs
 from astropy.coordinates import Distance
 
-from mphot import generate_system_response, get_precision
+from mphot import core
 
 
 ### PARAMETERS
@@ -291,7 +291,7 @@ def get_models(
 
     # get instrument data
     for filter_path in filter_paths:
-        name, system_response = generate_system_response(instrument_efficiency_path, filter_path)
+        name, system_response = core.generate_system_response(instrument_efficiency_path, filter_path)
         response = {
             "name" : name,
             "system_response" : system_response
@@ -332,7 +332,7 @@ def get_models(
 
 
 
-                    precision = get_precision(props_i, props_s, Teff, dist, extended=True)
+                    precision = core.get_precision(props_i, props_s, Teff, dist, extended=True)
 
                     # mphot may return quantity type
                     for i, el in enumerate(precision):
@@ -356,7 +356,7 @@ def get_models(
 
                     if precision_curve:
                         for i in N_stars:
-                            image_precision_i, binned_precision_i, components_i = get_precision(props_i, props_s, Teff, dist, exp_time=exp_time, N_star=i, extended=True)
+                            image_precision_i, binned_precision_i, components_i = core.get_precision(props_i, props_s, Teff, dist, exp_time=exp_time, N_star=i, extended=True)
                             
                             # mphot may return quantity type
                             for key in binned_precision_i.keys():
